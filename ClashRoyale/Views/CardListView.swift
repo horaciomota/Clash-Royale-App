9ca14ccae0c639b40c300fe3a9d0cb48c761c1d4
Horacio Mota <horaciomota93@gmail.com>
2024-02-26 19:51:06 +0000

    Organize project structure: Added and moved folders
    Created a more organized folder structure to better separate concerns and improve maintainability:
    - Added Models, Views, ViewModels, Services, Helpers, Resources, Extensions, and Supporting Files folders
    - Moved relevant files to their respective folders
    - This restructuring will help in better organizing and maintaining the codebase.

---
+++ ClashRoyale/Views/CardListView.swift
@@ -0,0 +1,85 @@
+//
+//  CardListView.swift
+//  ClashRoyale
+//
+//  Created by Horacio Mota on 25/02/24.
+//
+
+import SwiftUI
+
+struct CardListView: View {
+    @State private var cards: [Card] = []
+    @State private var selectedCard: Card? = nil
+    @State private var isLoading = false
+
+    let gridItems = [GridItem(.flexible(), spacing: 0), GridItem(.flexible(), spacing: 0), GridItem(.flexible(), spacing: 0)]
+
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: gridItems, spacing: 0) {
+                ForEach(cards) { card in
+                    Button(action: {
+                        selectedCard = card
+                    }) {
+                        ZStack(alignment: .topTrailing) {
+                            // Exibição da imagem da carta
+                            AsyncImage(url: URL(string: card.iconUrls.medium)) { phase in
+                                switch phase {
+                                case let .success(image):
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 110)
+                                        .cornerRadius(1)
+                                default:
+                                    ProgressView()
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                        .frame(width: 110)
+                                }
+                            }
+
+                            // Número de mana
+                            Circle()
+                                .foregroundColor(.purple)
+                                .frame(width: 40, height: 30)
+                                .overlay(
+                                    Text("\(card.elixirCost != nil ? "\(card.elixirCost!)" : "N/A")")
+                                        .font(.caption)
+                                        .foregroundColor(.white)
+                                )
+                                .offset(x: -80, y: 0)
+                        }
+                        .padding()
+                        .cornerRadius(8)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .padding()
+            .navigationTitle("Cartas")
+        }
+        .onAppear {
+            Task {
+                await fetchCards()
+            }
+        }
+    }
+
+    // Funcao usada para dar fetch nas cartas na view principal
+    func fetchCards() async {
+        isLoading = true
+        do {
+            let api = ClashRoyaleAPI()
+            cards = try await api.fetchAllCards()
+        } catch {
+            print("Erro ao buscar as cartas: \(error)")
+        }
+        isLoading = false
+    }
+}
+
+struct CardListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardListView()
+    }
+}
